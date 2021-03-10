6607b04210aea09e2c9b31463ca0aa0afa1103a5
alessandro <m.cherubini52@gmail.com>
2021-03-10 14:32:36 +0100

    Aggiungere i file di progetto.

---
+++ EserciziLibrary/GestioneTest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EserciziLibrary
+{
+    public class GestioneTest
+    {
+
+    }
+}
---
+++ EserciziLibraryTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EserciziLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
